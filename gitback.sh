e3e87155409034384913f4f4a1fedee74f4afdef
youzhilane <yixy@users.noreply.github.com>
2022-05-17 07:19:08 +0800

    Update gitback.sh

--- gitback.sh
+++ gitback.sh
@@ -1,6 +1,10 @@
 #/bin/bash
-DIR="$1/`date +%s`"
-mkdir -p $DIR
+DATE=`date +"%Y%m%d-%H%M%S.%s"`
+DIR="$1/$DATE"
+mkdir $DIR
 cd $DIR
 REPOS=`curl -s https://api.github.com/users/yixy/repos | grep clone_url | sed 's/.*\(https:\/\/github.com\/yixy\/.*\.git\)",/git clone \1/' > clone.sh`
 sh clone.sh
+cd ..
+tar -zcvf $DATE.tar.gz $DATE
+mv -f $DATE /tmp
